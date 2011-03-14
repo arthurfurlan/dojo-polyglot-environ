15d05591ef5b15e3eda1c57a6567a1999bd561b1
Arthur Furlan <afurlan@afurlan.org>
2011-03-13 22:26:57 -0300

    Fix the problem of non-testcase classes.

--- templates/php.php
+++ templates/php.php
@@ -1,15 +1,12 @@
-<?php
+#!/usr/bin/env php
+#
 # automatically created by Dojo Polyglot Environ on DATETIME
 # https://github.com/afurlan/dojo-polyglot-environ
+<?php
 
 require_once 'PHPUnit/Framework.php';
 
-class CLASSNAME {
-    function __construct() {
-    } 
-}
-
-class CLASSNAME_Test extends PHPUnit_Framework_TestCase {
+class CLASSNAME extends PHPUnit_Framework_TestCase {
     public function testConstruct() {
         $this->assertNotEquals(new CLASSNAME, null);
     }
