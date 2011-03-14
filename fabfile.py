cb16ea7fa6d7977e1e913135a59dafdd4f92c3cf
Arthur Furlan <afurlan@afurlan.org>
2011-03-14 01:56:03 -0300

    Add the "execute" command.

--- fabfile.py
+++ fabfile.py
@@ -17,23 +17,43 @@ import os, glob, shutil, datetime
 
 TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
+def _get_template_files(name='*'):
+    return glob.glob(os.path.join(TEMPLATES_DIR, name + '.*'))
+
 def create(lang, name):
     ''' Creates the new dojo file '''
 
     # check if the language is supported... it means, check if there is
     # an existent template for this language in the TEMPLATES_DIR
-    template = os.path.join(TEMPLATES_DIR, lang)
-    template = glob.glob(template + '.*')
+    template = _get_template_files(lang)
     if not template:
-        print 'ERROR: Language not supported.'
+        print 'ERROR: Language "%s" not supported.' % lang
         return
-    
-    dtstr = datetime.datetime.now().strftime('%Y-%m-%d %T')
-
+   
     # create the output file and rename to the same name of the class
+    dt_str = datetime.datetime.now().strftime('%Y-%m-%d %T')
     output = os.path.basename(template[0])
     output = output.replace(lang, name, 1).lower()
     shutil.copy(template[0], output)
     local(r"sed -i 's/CLASSNAME/%s/g' %s" % (name, output))
-    local(r"sed -i 's/DATETIME/%s/g' %s" % (dtstr, output))
+    local(r"sed -i 's/DATETIME/%s/g' %s" % (dt_str, output))
     local(r"chmod u+x %s" % output)
+
+def execute(fname, lang=None):
+    ''' Execute the file in order to check if the tests passed '''
+
+    # create dict of "extensions vs. languages"
+    lang_ext = {}
+    for t in _get_template_files():
+        l, e = os.path.basename(t).split('.')
+        lang_ext[e] = l
+
+    # check if the extension is supported
+    extension = fname.split('.')[-1]
+    if extension not in lang_ext and not lang:
+        print 'ERROR: Extension "%s" not supported.' % extension
+        return
+    
+    if '/' not in fname:
+        fname = './' + fname
+    local(fname)
