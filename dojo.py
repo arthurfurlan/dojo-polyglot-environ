a606414ee21d5e9ee1dac444cac8efc0be6921ee
Arthur Furlan <afurlan@afurlan.org>
2011-03-15 11:34:03 -0300

    Move isolated functions to became part of the DojoPolyglotEnviron class.

--- dojo.py
+++ dojo.py
@@ -25,65 +25,68 @@ ICON_FILES = {
     'success': os.path.join(ROOT_DIR, 'images/success.png'),
 }
 
-def _get_template_files(name='*'):
-    return glob.glob(os.path.join(TMPL_DIR, name + '.*'))
+class DojoPolyglotEnviron:
 
-def create(lang, name):
-    ''' Creates the new dojo file '''
+    def _get_template_files(self, name='*'):
+        return glob.glob(os.path.join(TMPL_DIR, name + '.*'))
 
-    # check if the language is supported... it means, check if there is
-    # an existent template for this language in the TMPL_DIR
-    template = _get_template_files(lang)
-    if not template:
-        print 'ERROR: Language "%s" not supported.' % lang
-        return
-   
-    # create the output file name based on language
-    output = os.path.basename(template[0])
-    output = output.replace(lang, name, 1).lower()
+    def _runtests_notify(self, command_args):
+        notify_args = ['notify-send', '-i']
+        retcode = subprocess.call(command_args)
+        if not retcode:
+            notify_args.append(ICON_FILES['success'])
+            notify_args.append('Tests passed.')
+        else:
+            notify_args.append(ICON_FILES['error'])
+            notify_args.append('Tests failed.')
+        subprocess.call(notify_args)
 
-    # write the content of the output file
-    ft, fo = open(template[0]), open(output, 'w+')
-    for line in ft:
-        line = line.replace('CLASSNAME', name)
-        line = line.replace('DATETIME', datetime.now().strftime('%Y-%m-%d %T'))
-        fo.write(line)
-    fo.close()
-    os.chmod(output, 0744)
+    def runtests(self, fname, lang=None):
+        ''' Execute the file in order to check if the tests passed '''
 
-def _runtests_notify(command_args):
-    notify_args = ['notify-send', '-i']
-    retcode = subprocess.call(command_args)
-    if not retcode:
-        notify_args.append(ICON_FILES['success'])
-        notify_args.append('Tests passed.')
-    else:
-        notify_args.append(ICON_FILES['error'])
-        notify_args.append('Tests failed.')
-    subprocess.call(notify_args)
+        # create dict of "extensions vs. languages"
+        lang_ext = {}
+        for t in self._get_template_files():
+            l, e = os.path.basename(t).split('.')
+            lang_ext[e] = l
 
-def runtests(fname, lang=None):
-    ''' Execute the file in order to check if the tests passed '''
+        # check if the file extension is supported
+        extension = fname.split('.')[-1]
+        if extension not in lang_ext and not lang:
+            print 'ERROR: Extension "%s" not supported.' % extension
+            return
+        elif not lang:
+            lang = lang_ext[extension]
+        
+        if '/' not in fname:
+            fname = './' + fname
+        command_args = [ lang, fname]
+        self._runtests_notify(command_args)
 
-    # create dict of "extensions vs. languages"
-    lang_ext = {}
-    for t in _get_template_files():
-        l, e = os.path.basename(t).split('.')
-        lang_ext[e] = l
+    def create(self, lang, name):
+        ''' Creates the new dojo file '''
 
-    # check if the file extension is supported
-    extension = fname.split('.')[-1]
-    if extension not in lang_ext and not lang:
-        print 'ERROR: Extension "%s" not supported.' % extension
-        return
-    elif not lang:
-        lang = lang_ext[extension]
-    
-    if '/' not in fname:
-        fname = './' + fname
-    command_args = [ lang, fname]
-    _runtests_notify(command_args)
+        # check if the language is supported... it means, check if there is
+        # an existent template for this language in the TMPL_DIR
+        template = self._get_template_files(lang)
+        if not template:
+            print 'ERROR: Language "%s" not supported.' % lang
+            return
+       
+        # create the output file name based on language
+        output = os.path.basename(template[0])
+        output = output.replace(lang, name, 1).lower()
+
+        # write the content of the output file
+        ft, fo = open(template[0]), open(output, 'w+')
+        for line in ft:
+            line = line.replace('CLASSNAME', name)
+            line = line.replace('DATETIME', datetime.now().strftime('%Y-%m-%d %T'))
+            fo.write(line)
+        fo.close()
+        os.chmod(output, 0744)
 
 if __name__ == '__main__':
-    #create('python', 'abcd')
-    runtests('abcd.py')
+    dj = DojoPolyglotEnviron()
+    dj.create('python', 'abcd')
+    #runtests('abcd.py')
